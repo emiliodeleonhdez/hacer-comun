5428cde17e9c203a5e02a39547b532ea9ec11e6a
Emilio De Leon <emiliodeleon.contact@gmail.com>
2023-04-07 23:18:48 -0600

    added icons and start to work responsive

--- pages/products/product-detail/[id].js
+++ pages/products/product-detail/[id].js
@@ -2,6 +2,9 @@ import React from "react"
 import { useRouter } from "next/router"
 import Image from "next/image"
 
+import { AiOutlinePlusCircle } from "react-icons/ai"
+import { AiOutlineMinusCircle } from "react-icons/ai"
+
 const ProductDetail = () => {
   const router = useRouter()
   const { id } = router.query
@@ -12,11 +15,13 @@ const ProductDetail = () => {
     product_title: "I'm a super product Title",
     product_category: "random category",
     stock: 25,
-    variant: ["variant 1", "variant 2", "variant 3"]
+    variants: ["variant 1", "variant 2", "variant 3"],
+    product_description:
+      "Discover the beauty of Oaxacan craftsmanship with our exquisite craft product, handmade with love by skilled artisans from the state of Oaxaca in Mexico."
   }
 
   return (
-    <main className="flex h-full items-center justify-center">
+    <main className="flex flex-col md:flex-row h-full items-center justify-center">
       <aside className="flex flex-col p-8">
         <Image
           src={product.img_url}
@@ -25,12 +30,27 @@ const ProductDetail = () => {
           height={500}
         />
       </aside>
-      <aside className="flex flex-col p-8">
+      <aside className="product-detail flex flex-col w-3/5 p-8 h-4/5 justify-between">
         <h1 className="text-5xl">{product.product_title}</h1>
         <span>Categoría: {product.product_category} </span>
         <span>Cantidad</span>
-        {/* ----counter---- */}
-        {/* ----variant---- */}
+        <div className="qty-counter w-20 flex justify-between items-center">
+          <AiOutlinePlusCircle className="hover:border-slate-500 hover:text-rose-900" />
+          <span>{product.stock}</span>
+          <AiOutlineMinusCircle className="hover:border-slate-500 hover:text-rose-900" />
+        </div>
+        <div className="product-variant flex w-2/5 justify-between">
+          {product.variants.map((variant) => (
+            <div key={id} className="bg-slate-200 p-2">
+              {variant}
+            </div>
+          ))}
+        </div>
+        <button className="border-solid border border-slate-800 p-2 rounded-2xl hover:border-slate-500 hover:text-rose-900">
+          <span>Añadir al carrito</span>
+        </button>
+        <span>Descripción:</span>
+        <p>{product.product_description}</p>
       </aside>
     </main>
   )
