4e15dc21e1b68e40c80bbf05ae15169ce63bce09
Emilio De Leon <emiliodeleon.contact@gmail.com>
2023-04-07 20:10:28 -0600

    adding add to cart button and hover effect

--- components/ProductCard/ProductCard.js
+++ components/ProductCard/ProductCard.js
@@ -5,12 +5,17 @@ const ProductCard = ({ cardContent }) => {
   const { title, sub, price, img_url } = cardContent
 
   return (
-    <div className="w-64 h-80  p-7 m-4 border-solid border border-slate-800 rounded-2xl flex justify-center">
+    <div className="w-64 h-96  p-7 m-4 border-solid border border-slate-800 rounded-2xl flex justify-center">
       <div className="flex flex-col justify-between ">
         <Image src={img_url} alt="hacer-comun" width={500} height={500} />
         <h2 className="font-bold text-center">{title}</h2>
         <span className="text-center">{sub}</span>
         <span className="text-left">{price}</span>
+        <div className="flex justify-end">
+          <button className="border-solid border border-slate-800 p-2 rounded-2xl hover:border-slate-500">
+            <span className="hover:text-rose-900">Añadir al carrito</span>
+          </button>
+        </div>
       </div>
     </div>
   )
