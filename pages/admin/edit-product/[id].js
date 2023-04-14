a7c3a05d496a5c5dda93f99c28742a7a7d57bb9e
Emilio De Leon <emiliodeleon.contact@gmail.com>
2023-04-13 19:46:49 -0600

    continue to work on edit product

--- pages/admin/edit-product/[id].js
+++ pages/admin/edit-product/[id].js
@@ -22,7 +22,7 @@ const EditProduct = () => {
       "Este hermoso textil bordado a mano en Oaxaca es más que una simple pieza decorativa. Cada hilo y cada puntada cuentan una historia de la tradición y la cultura de las comunidades indígenas de la sierra. Lo que hace especial a este producto es que, al adquirirlo, estás contribuyendo a fomentar el comercio justo y apoyar directamente a las comunidades de la región. De esta manera, no solo estás adquiriendo una obra de arte única, sino que también estás haciendo una diferencia positiva en la vida de las personas que la crearon."
   }
 
-  const [productUpdate, setProductUpdate] = useState(product)
+  const [productUpdate, setProductUpdate] = useState({})
 
   const handleEnterKey = (e) => {
     if (e.key === "Enter") {
@@ -37,7 +37,7 @@ const EditProduct = () => {
       [e.target.name]: e.target.value
     })
   }
-
+  console.log(productUpdate)
   return (
     <div className="flex flex-col md:flex-row h-full items-center justify-center">
       <aside className="flex flex-col p-8">
@@ -54,31 +54,39 @@ const EditProduct = () => {
             className="text-center md:text-start"
             type="text"
             name="product_title"
-            value={productUpdate.product_title}
+            value={
+              productUpdate.product_title
+                ? productUpdate.product_title
+                : product.product_title
+            }
             onChange={handleUpdateProduct}
             onKeyDown={(e) => {
               handleEnterKey(e)
             }}
           />
         </h1>
-        <span className="my-3">
-          Categoría:{" "}
+        <div>
+          <span>Categoría: </span>
           <input
             type="text"
             name="product_category"
-            value={productUpdate.product_category}
+            value={
+              productUpdate.product_category
+                ? productUpdate.product_category
+                : product.product_category
+            }
             onChange={handleUpdateProduct}
             onKeyDown={(e) => {
               handleEnterKey(e)
             }}
           />
-        </span>
-        <span className="mb-3">
+        </div>
+        <span className="mb-3 text-center md:text-start">
           $
           <input
             type="text"
             name="price"
-            value={productUpdate.price}
+            value={productUpdate.price ? productUpdate.price : product.price}
             onChange={handleUpdateProduct}
             onKeyDown={(e) => {
               handleEnterKey(e)
@@ -86,9 +94,19 @@ const EditProduct = () => {
           />
         </span>
         <span>Stock</span>
+
         <div className="qty-counter w-20 flex justify-between items-center">
           <AiOutlinePlusCircle className="hover:border-slate-500 hover:text-rose-900" />
-          <span>{product.stock}</span>
+          <input
+            type="number"
+            min={0}
+            name="stock"
+            value={productUpdate.stock ? productUpdate.stock : product.stock}
+            onChange={handleUpdateProduct}
+            onKeyDown={(e) => {
+              handleEnterKey(e)
+            }}
+          />
           <AiOutlineMinusCircle className="hover:border-slate-500 hover:text-rose-900" />
         </div>
         <div className="product-variant flex m-5 md:m-0 md:my-3">
@@ -102,7 +120,22 @@ const EditProduct = () => {
           ))}
         </div>
         <span className="my-3">Descripción:</span>
-        <p>{product.product_description}</p>
+        <div className="w-full h-65 overflow-y-auto">
+          <input
+            type="text"
+            name=" product_description"
+            className="w-full"
+            value={
+              productUpdate.product_description
+                ? productUpdate.product_description
+                : product.product_description
+            }
+            onChange={handleUpdateProduct}
+            onKeyDown={(e) => {
+              handleEnterKey(e)
+            }}
+          />
+        </div>
       </aside>
     </div>
   )
