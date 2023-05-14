cfc24772ab0c25e957eb8e48b14b52794ccd943c
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2023-05-14 12:26:30 -0600

    Changes in middleware completed relative path

--- middleware.js
+++ middleware.js
@@ -1,22 +1,23 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const apiUrl = `https://api-rest-hacer-comun-production.up.railway.app`;
+  const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
   const token =
-    "syJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDUwYTM0Yzk2ODExM2E5MmQ4MDhlN2IiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjg0MDM1MTYwLCJleHAiOjE2ODQwMzg3NjB9.8seYwq4di6zdJlqEIz-POliK5hW3HlYOxjz1heKwwGA";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDUwYTM0Yzk2ODExM2E5MmQ4MDhlN2IiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjg0MDg4MTc5LCJleHAiOjE2ODQwOTE3Nzl9.MTBZioSNEj7YDIty2DW_BIhiMRhq2H5hlYnUyAAFT7Q";
 
   const validateToken = await fetch(`${apiUrl}/users/auth/validate-token`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  }).then((response) => {
-      console.log("response status code: ",response.status)
-    if (response.status !== 200) {
-        console.log(request.url)
-        return NextResponse.redirect(new URL('/login', request.url));
-    }
-    return NextResponse.next();
   });
+  if (validateToken.status !== 200) {
+    console.log("Token not valid");
+    const loginUrl = request.nextUrl.clone();
+    loginUrl.pathname = "/login";
+    return NextResponse.redirect(loginUrl);
+  }
+  console.log("Token valid")
+  return NextResponse.next();
 }
 
 export const config = {
